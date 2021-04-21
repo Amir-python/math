2d8740e10863dcade1ccc01bc17e24c0b1005b4a
Amir-python <82511775+Amir-python@users.noreply.github.com>
2021-04-21 09:29:36 +0600

    Add files via upload

---
+++ ммм.py
@@ -0,0 +1,9 @@
+from random import *
+a = randint(1, 15)
+print('добро пожаловать в казино')
+b = int(input('выбери число от 1 до 15: '))
+if a == b :
+    print('ты выйграл')
+else:
+    print('ты проигал душу')
+
